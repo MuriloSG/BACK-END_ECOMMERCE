f9b16d724bdbfebb4e3630c707eeed526676320e
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2024-01-24 09:38:40 -0300

    Creating migration, entity and repository Usertokens

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+class UserToken {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
+
+export default UserToken;
