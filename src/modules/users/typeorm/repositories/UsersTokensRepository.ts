ce2be40926a8b2cb701527d8d846faf6772a9933
MuriloSG <muriloprojetosti@gmail.com>
2024-03-13 16:27:46 -0300

    refactor promise in repository Userstoken method gererate

--- src/modules/users/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/users/typeorm/repositories/UsersTokensRepository.ts
@@ -14,7 +14,7 @@ export class UsersTokenRepository extends Repository<UserToken>{
   }
 
   //Createe token.
-  public async generate(user_id: string): Promise<UserToken | undefined> {
+  public async generate(user_id: string): Promise<UserToken> {
     const userToken = this.create({
       user_id,
     });
