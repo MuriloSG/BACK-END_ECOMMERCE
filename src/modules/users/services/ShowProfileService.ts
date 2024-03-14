4be2ba792adcd985f468418df533bb76e4d1e6f7
MuriloSG <muriloprojetosti@gmail.com>
2024-03-14 17:54:03 -0300

    Refactor service

--- src/modules/users/services/ShowProfileService.ts
+++ src/modules/users/services/ShowProfileService.ts
@@ -4,14 +4,14 @@ import { UsersRepository } from "../typeorm/repositories/UsersRepository";
 import AppError from "@shared/errors/AppError";
 
 interface IRequest {
-  user_Id: string;
+  user_id: string;
 }
 
 class ShowProfileService {
-  public async execute({ user_Id }: IRequest): Promise<User> {
+  public async execute({ user_id }: IRequest): Promise<User> {
 
     const usersRepository = getCustomRepository(UsersRepository);
-    const user = await usersRepository.findById(user_Id)
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found', 404);
