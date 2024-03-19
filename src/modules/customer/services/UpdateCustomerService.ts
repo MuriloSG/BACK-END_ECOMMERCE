6fc0f7cfb9155bef917f832c08adf43c27dce611
MuriloSG <muriloprojetosti@gmail.com>
2024-03-19 10:08:04 -0300

    Creting update customer service

---
+++ src/modules/customer/services/UpdateCustomerService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import Customer from "../typeorm/entities/Customer";
+import { CustomerRepository } from "../typeorm/repositories/CustomersRepositoriy";
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateProfileService {
+  public async execute({ id, name, email}: IRequest): Promise<Customer> {
+
+    const customerRepository = getCustomRepository(CustomerRepository);
+    const customer = await customerRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('User not found', 404);
+    }
+
+    const emailExists = await customerRepository.findByEmail(email);
+
+    if (emailExists && email !== customer.email) {
+      throw new AppError("Email already exists");
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    await customerRepository.save(customer);
+
+    return customer;
+  }
+}
+
+export default UpdateProfileService;
