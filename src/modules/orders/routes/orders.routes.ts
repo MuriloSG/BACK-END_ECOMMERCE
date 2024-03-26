fb46f36dc16deebecfb48e0754b74167f0804e51
MuriloSG <muriloprojetosti@gmail.com>
2024-03-26 08:56:42 -0300

    refactor

--- src/modules/orders/routes/orders.routes.ts
+++ src/modules/orders/routes/orders.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import {celebrate, Joi ,Segments } from "celebrate";
 import OrdersController from "../controllers/OrdersController";
+import isAuthenticate from "@shared/http/middlewares/isAuthenticate";
 
 const ordersRouter = Router();
 const ordersController = new OrdersController();
+
+ordersRouter.use(isAuthenticate);
 
 ordersRouter.get(
   "/:id",
